91c12e9608c42fb177be7603af4e0b8d1dac0395
ironmanmark23 <ironmanmark2301@gmail.com>
2018-08-31 20:16:38 +0530

    commit new changes....

---
+++ quicksort.py
@@ -0,0 +1,28 @@
+##partition algorithm
+def partition(arr,low,high):
+    i = (low-1)
+
+    pivot =arr[high]
+    for j in range(low, high):
+
+        if arr[j]<= pivot:
+            i = i+ 1
+            arr[i],arr[j] = arr[j],arr[i]
+    arr[i+1],arr[high] = arr[high],arr[i+1]
+    return(i+1)
+##sorting algorithm
+def quickSort(arr,low,high):
+    if low< high:
+
+        pi = partition(arr,low,high)
+
+        quickSort(arr,low, pi-1)
+        quickSort(arr, pi + 1,high)
+
+arr = [1,2,22,33,6,5,10,72,89]
+n = len(arr)
+quickSort(arr,0,n-1)
+print("Sorted array is")
+for i in range(n):
+    print ("%d" %arr[i])
+    
